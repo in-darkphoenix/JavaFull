c76a0f00a5afd4194ba371d432bc60e12b159239
Ankit <ph.official.ankit@gmail.com>
2019-10-20 22:48:33 +0530

    Decimal to binary conversion

---
+++ src/basics/DecimalToBinary.java
@@ -0,0 +1,27 @@
+package basics;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int multiplier = 1;
+		int ans = 0;
+
+		while (n != 0) {
+
+			int rem = n % 2;
+
+			ans = ans + rem * multiplier;
+
+			multiplier = multiplier * 10;
+			n = n / 2;
+		}
+
+		System.out.println(ans);
+
+	}
+
+}
